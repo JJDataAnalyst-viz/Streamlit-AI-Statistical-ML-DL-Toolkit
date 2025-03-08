6a9f32da6f85aa9fbda89dc25d5f8b6a0e8b87f5
SciOpsEngineer <kubajedrych100@gmail.com>
2025-03-08 17:55:08 +0100

    Created ML Pages

--- Home.py
+++ Home.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from OOP_statistical_app import *
 from home_app import home
+from about_app import about
 
 st.set_page_config(layout="wide")
 def main():
@@ -12,6 +13,8 @@ def main():
         home()
     if choice == 'Statistical analysis':
             statistical_analysis()
+    if choice == 'About':
+         about()
 if __name__ =="__main__":
     main()  
 
